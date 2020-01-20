2c0aec3118f6c386d5be1a9f1c7557e8bb7dbf6b
Elena Chypenko <elenachypenko@gmail.com>
2020-01-20 15:31:22 +0200

    Redux-counter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,44 @@
-console.log('Hello world! :)');
+const counterValue = document.getElementById('counter');
+const counterIncrementBtnNode = document.getElementById('counter-increment-btn');
+const counterDecrementBtnNode = document.getElementById('counter-decrement-btn')
+
+function createCounterIncrementAction() {
+    return {
+        type: 'INCREMENT',
+    };
+}
+
+function createCounterDecrementAction() {
+    return {
+        type: 'DECREMENT',
+    };
+}
+
+const store = window.Redux.createStore(counterReducer);
+window.store = store;
+
+store.subscribe(() => {
+    const state = store.getState();
+    console.log('subscribe', state);
+    counterValue.innerText = state;
+});
+
+counterIncrementBtnNode.addEventListener('click', () => {
+    store.dispatch(createCounterIncrementAction())
+} );
+
+counterDecrementBtnNode.addEventListener('click', () => {
+    store.dispatch(createCounterDecrementAction())
+} );
+
+function counterReducer(state = 0, action) {
+    if (action.type === 'INCREMENT') {
+        return state + 1;
+    }
+
+    if (action.type === 'DECREMENT') {
+        return state - 1;
+    }
+
+    return state;
+}
